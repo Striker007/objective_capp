58ae15042f0d4bc41da1857aace36d59f50786f8
Striker007 <vet.it08@gmail.com>
2015-11-24 17:27:46 +0200

    Initial Commit

---
+++ objective_capp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objective_capp
+//
+//  Created by Vitalii PHP on 11/24/15.
+//  Copyright © 2015 monsters. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
