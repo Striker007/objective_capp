43e01f4e50ee991bed14a8d47a1b901b29099efb
Striker007 <vet.it08@gmail.com>
2015-11-24 18:37:42 +0200

    init

--- objective_capp/main.m
+++ objective_capp/main.m
@@ -2,16 +2,30 @@
 //  main.m
 //  objective_capp
 //
-//  Created by Vitalii PHP on 11/24/15.
+//  Created by Vitalii (Striker007) on 11/24/15.
 //  Copyright © 2015 monsters. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#include <errno.h>
+#include <sys/sysctl.h>
+
 
 int main(int argc, const char * argv[]) {
+    
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        char str[256];
+        size_t size = sizeof(str);
+        
+        sysctlbyname("kern.osrelease", str, &size, NULL, 0);
+        
+        if (!strcmp(str, "")) {
+            strcpy(str, "unknown");
+        }
+        
+        NSLog(@"\n OS ver. %s \n", str);
+
     }
     return 0;
 }
